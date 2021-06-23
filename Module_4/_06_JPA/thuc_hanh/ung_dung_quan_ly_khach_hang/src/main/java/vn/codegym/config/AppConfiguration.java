9817311738ea2a161ad50e8e087a7f3698cb50cf
nhkhanh6398 <nhkhanh6398@gmail.com>
2021-06-23 22:40:08 +0700

    done

--- Module_4/_06_JPA/thuc_hanh/ung_dung_quan_ly_khach_hang/src/main/java/vn/codegym/config/AppConfiguration.java
+++ Module_4/_06_JPA/thuc_hanh/ung_dung_quan_ly_khach_hang/src/main/java/vn/codegym/config/AppConfiguration.java
@@ -112,7 +112,7 @@ public class AppConfiguration extends WebMvcConfigurerAdapter implements Applica
 
     Properties additionalProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "create");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
         properties.setProperty("show_sql", "false");
         return properties;
