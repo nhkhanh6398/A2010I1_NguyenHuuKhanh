766e341e394eeead342c7305951129e56feba2eb
nhkhanh6398 <nhkhanh6398@gmail.com>
2021-07-07 23:46:04 +0700

    update

---
+++ Module_4/_09_AOP&Exception/thuc_hanh/cms/src/main/java/cg/wbd/grandemonstration/concern/Logger.java
@@ -0,0 +1,10 @@
+package cg.wbd.grandemonstration.concern;
+
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class Logger {
+    @AfterThrowing(pointcut = "execution(public * cg.wbd.grandemonstration.service.CustomerService.findAll(..))")
+    public void log() {
+}}
