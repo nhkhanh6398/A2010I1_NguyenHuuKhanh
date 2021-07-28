e65281f339313c88ce1fb74c7ab5a912e8d0608e
nhkhanh6398 <nhkhanh6398@gmail.com>
2021-07-28 21:30:29 +0700

    done

---
+++ Module_4/_13_I18n/thuc_hanh/ung_dung_blog_quoc_te/src/main/java/vn/codegym/controller/LoginController.java
@@ -0,0 +1,31 @@
+package vn.codegym.controller;
+
+;
+import org.springframework.boot.autoconfigure.ldap.embedded.EmbeddedLdapProperties;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import vn.codegym.model.User;
+
+@Controller
+@SessionAttributes("user")
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(@ModelAttribute("credential") EmbeddedLdapProperties.Credential credential) {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String dashboard(@ModelAttribute("credential") EmbeddedLdapProperties.Credential credential, Model model) {
+        User user = new User();
+        user.setUserName(credential.getUsername());
+        model.addAttribute("user", user);
+        return "redirect:dashboard";
+    }
+
+}
