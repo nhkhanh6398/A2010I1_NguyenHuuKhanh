98748e45ed3fe78d3974199b81940f19c6175dd5
nhkhanh6398 <nhkhanh6398@gmail.com>
2021-08-25 21:21:07 +0700

    update 25/8

---
+++ Module_05/4_angular_directive/thuc_hanh/Hackernews/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  article = {
+    title: 'The Evolution of Async JavaScript: From Callbacks, to Promises, to Async/Await',
+    url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40'
+  };
+
+  updateArticle() {
+    this.article.title = (document.getElementById('article-title')as HTMLInputElement).value;
+    this.article.url = (document.getElementById('article-url')as HTMLInputElement).value;
+  }
+}
